6dbba8f64045152a6b0200ac2b5998590b65a49c
mikearaya <mikaelaraya12@gmail.com>
2019-03-16 10:17:43 +0300

    completed news form

--- myworkspace/apps/ilo-admin/src/app/features/news/news-form/news-form.component.ts
+++ myworkspace/apps/ilo-admin/src/app/features/news/news-form/news-form.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsApiService } from '../news-api.service';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  FormArray
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { SystemApiService } from '../../../system-api.service';
+import { NewsViewModel, NewsModel } from '../news-data.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'bionic-news-form',
@@ -6,7 +18,155 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-form.component.css']
 })
 export class NewsFormComponent implements OnInit {
-  constructor() {}
+  public headerText: Object = [
+    { text: 'Enlish Language (default)' },
+    { text: 'Other Languages' }
+  ];
+  public newsLocaleForm: FormGroup;
+  public newsForm: FormGroup;
+  private newsId: number;
+  public languages: any;
+  public isUpdate: Boolean;
 
-  ngOnInit() {}
+  constructor(
+    private newsApi: NewsApiService,
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private systemConf: SystemApiService
+  ) {
+    this.createForm();
+    this.createLocaleForm();
+  }
+
+  ngOnInit() {
+    this.newsId = +this.activatedRoute.snapshot.paramMap.get('newsId');
+
+    if (this.newsId) {
+      this.isUpdate = true;
+      this.newsApi
+        .getNewsById(this.newsId)
+        .subscribe((data: NewsViewModel) => this.initializeForm(data));
+    }
+  }
+
+  private createForm(): void {
+    this.newsForm = this.formBuilder.group({
+      header: ['', Validators.required],
+      content: ['', Validators.required],
+      audio: [''],
+      image: [''],
+      video: ['']
+    });
+  }
+
+  private createLocaleForm(): void {
+    this.newsLocaleForm = this.formBuilder.group({
+      newsLocales: this.formBuilder.array([this.generateLocaleForm()])
+    });
+  }
+
+  get header(): FormControl {
+    return this.newsForm.get('header') as FormControl;
+  }
+
+  get newsContent(): FormControl {
+    return this.newsForm.get('content') as FormControl;
+  }
+
+  get audio(): FormControl {
+    return this.newsForm.get('audio') as FormControl;
+  }
+
+  get video(): FormControl {
+    return this.newsForm.get('video') as FormControl;
+  }
+
+  get image(): FormControl {
+    return this.newsForm.get('image') as FormControl;
+  }
+
+  get newsLocales(): FormArray {
+    return this.newsLocaleForm.get('newsLocales') as FormArray;
+  }
+
+  addLocale(): void {
+    this.newsLocales.controls.push(this.generateLocaleForm());
+  }
+
+  deleteLocale(index: number): void {
+    this.newsLocales.removeAt(index);
+  }
+
+  private initializeForm(news: NewsModel): void {
+    this.newsForm = this.formBuilder.group({
+      header: [news.header, Validators.required],
+      content: [news.content, Validators.required],
+      audio: [news.audio],
+      image: [news.image],
+      video: [news.video]
+    });
+  }
+
+  private generateLocaleForm(): FormGroup {
+    return this.formBuilder.group({
+      header: ['', Validators.required],
+      content: ['', Validators.required],
+      locale: ['', Validators.required],
+      audio: [''],
+      image: [''],
+      video: ['']
+    });
+  }
+
+  onSubmit(): void {
+    const formData = this.prepareFormData();
+
+    if (formData) {
+      if (this.isUpdate) {
+        this.newsApi
+          .updateNews(formData)
+          .subscribe(
+            () => alert('News updated successfully'),
+            (error: HttpErrorResponse) =>
+              alert('Failed while attempting to update news try again')
+          );
+      } else {
+        this.newsApi.createNews(formData).subscribe(
+          (data: any) => {
+            this.isUpdate = true;
+            this.newsId = data;
+            alert('New created successfully');
+          },
+          (error: HttpErrorResponse) => {
+            alert('Failed while creating new, try again');
+          }
+        );
+      }
+    }
+  }
+
+  private prepareFormData(): NewsModel | null {
+    if (this.newsForm.valid) {
+      if (this.isUpdate && this.newsId) {
+        return {
+          ID: this.newsId,
+          header: this.header.value,
+          content: this.newsContent.value,
+          audio: this.audio.value,
+          video: this.video.value,
+          image: this.image.value
+        };
+      } else {
+        return {
+          header: this.header.value,
+          content: this.newsContent.value,
+          audio: this.audio.value,
+          video: this.video.value,
+          image: this.image.value
+        };
+      }
+    } else {
+      return null;
+    }
+  }
 }
--- myworkspace/apps/ilo-admin/src/app/features/news/news.module.ts
+++ myworkspace/apps/ilo-admin/src/app/features/news/news.module.ts
@@ -6,10 +6,11 @@ import { SharedComponentModule } from '@bionic/shared-component';
 import { NewsViewComponent } from './news-view/news-view.component';
 import { NewsFormComponent } from './news-form/news-form.component';
 import { NewsApiService } from './news-api.service';
+import { TabModule } from '@syncfusion/ej2-angular-navigations';
 
 @NgModule({
   declarations: [NewsFormComponent, NewsViewComponent],
-  imports: [CommonModule, NewsRoutingModule, SharedComponentModule],
+  imports: [CommonModule, NewsRoutingModule, SharedComponentModule, TabModule],
   providers: [NewsApiService]
 })
 export class NewsModule {}
